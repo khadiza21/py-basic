ae46e645f7c0b14fc6d0151e738f6e8a4a5e6936
BK -40 <bkhadiza277@gmail.com>
2022-10-13 21:58:02 +0600

    args function parameters explore in python

--- funtion-list-set-tuple/function.py
+++ funtion-list-set-tuple/function.py
@@ -6,21 +6,41 @@ double_it(4)
 
 def add(num1,  num2):
     total = num1+ num2
-    print(total)
-add(23,17)
+    return (total)
+sum = add(23,17)
 
 # return function 
 def multiply (num1, num2):
-    result = num1 * num2
-    return result
+    result1 = num1 * num2
+    return result1
 output = multiply(2,4)
 print(output)
 
 
 # function reusability
-final_number = add(sum, output)
-print('final number ', final_number)
+print('final number ', add(sum, output) )
 
 
+
+# optional parameters and args
+def addition(num1, num2=0, num3=1, num4 = 2):
+    total = num1 + num2
+    print (num1, num2)
+    return total
+
+
+result2 = addition(12,14)
+result3 = addition(num2=12, num1 = 14)
+result4 = addition(45)
+
+# args
+def multiply_args(*numbers):
+    result = 1
+    for num in numbers:
+        result = result * num
+    return result
+
+final_result = multiply_args(2,3,2,3,4,2,2)
+print(final_result)
 
 
