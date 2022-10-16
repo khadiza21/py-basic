ce171e07ac4da143385426cb4cc4512e9c81cec6
khadiza21 <bibikhadiza474@gmail.com>
2022-10-16 21:54:33 +0600

    global and local variable scope , global keyword use

--- funtion-list-set-tuple/scope.py
+++ funtion-list-set-tuple/scope.py
@@ -3,11 +3,15 @@
 balance = 345
 remaining = 10
 def total_cost (price, quantity):
-     # set global variable's new value inside function ()
+    global remaining # set global variable's new value inside function ()
     vat = 45  #local
     cost = (price+vat )*quantity 
-    remaining = balance - cost
-    print(remaining)
+    remaining = balance-  cost 
+    print('remainging ',remaining)
     return cost 
 pay = total_cost(10,2)
 print(f'please pay: {pay}')
+print('remainging ',remaining)
+
+
+
