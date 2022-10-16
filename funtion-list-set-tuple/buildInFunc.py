1a15ecbe2f2918e7d3c717237ed642637522214b
khadiza21 <bibikhadiza474@gmail.com>
2022-10-16 22:43:56 +0600

    build in modules and build in function

---
+++ funtion-list-set-tuple/buildInFunc.py
@@ -0,0 +1,15 @@
+# import math 
+from math import sin, ceil
+import random
+import datetime as dt
+from unittest import result
+
+result1 = sin(30)
+result = ceil(2.13)
+print(result1)
+print(result)
+num1 = random.random()
+num2 = random.randint(49,61)
+winner = random.choice([3,4,6,34,62,17,61])
+print(winner)
+dt.MINYEAR
