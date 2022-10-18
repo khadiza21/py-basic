ce5fd77b3cf1301090cf91716cccf68897b39d78
khadiza21 <bibikhadiza474@gmail.com>
2022-10-18 19:49:39 +0600

    tuple in python

---
+++ list-tuple-dictionary-set/tuple.py
@@ -0,0 +1,18 @@
+numbers = [12,23,45,53,23,12,45]
+# tuple
+numbers_tuple = 12, 23,34,63,62,90,23
+nums_tuple = (12,23,64)
+
+print(numbers_tuple)
+print(numbers_tuple[4])
+print(nums_tuple)
+
+tuple2D = ([23,53,64],[34,62,])
+print(tuple2D)
+tuple2D[0][1]=33
+print(tuple2D)
+
+short_tuple =2,
+tuple_from_list = tuple(numbers)
+print(tuple_from_list)
+
