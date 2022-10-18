3b2ff5cc990b35ef0408f5e0e419eec0aae16c73
khadiza21 <bibikhadiza474@gmail.com>
2022-10-18 22:50:00 +0600

    loop on list in python

---
+++ list-tuple-dictionary-set/loop.py
@@ -0,0 +1,14 @@
+# list
+numbers = [12,23,53,64,34,89,29,51]
+total = sum(numbers) # built in function in python sum()
+print(total)
+total1=0
+for i in numbers:
+    total1 += i
+    print(i)
+print(total1)
+
+# set
+nums = {12,23,45,53,23,12,45}
+
+
